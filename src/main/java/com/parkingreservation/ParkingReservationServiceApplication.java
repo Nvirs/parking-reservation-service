f49645cb2db3ee97a82caa66cbbc2687e8e8f38b
Nvirs <ivasyukandras1@gmail.com>
2026-08-01 17:16:42 +0200

    initial parking reservation application

---
+++ src/main/java/com/parkingreservation/ParkingReservationServiceApplication.java
@@ -0,0 +1,12 @@
+package com.parkingreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingReservationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingReservationServiceApplication.class, args);
+    }
+}
