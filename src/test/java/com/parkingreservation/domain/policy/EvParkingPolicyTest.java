31a18eefb0e5d14fbfe459cdf05ba1921edd2dd4
Nvirs <ivasyukandras1@gmail.com>
2026-08-02 17:38:43 +0200

    electric vehicle owners test

--- src/test/java/com/parkingreservation/domain/policy/EvParkingPolicyTest.java
+++ src/test/java/com/parkingreservation/domain/policy/EvParkingPolicyTest.java
@@ -1,6 +1,15 @@
 package com.parkingreservation.domain.policy;
 
+import com.parkingreservation.domain.model.ParkingSpot;
 import com.parkingreservation.domain.model.ParkingType;
+import com.parkingreservation.domain.model.Reservation;
+import com.parkingreservation.domain.model.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class EvParkingPolicyTest extends ReservationPolicyContractTest {
 
@@ -17,5 +26,16 @@ class EvParkingPolicyTest extends ReservationPolicyContractTest {
     @Override
     protected ParkingType unsupportedType() {
         return ParkingType.STANDARD;
+    }
+
+    @Test
+    @DisplayName("rejects a request when the requester is not registered as an electric vehicle owner")
+    void rejectsWhenNotAnElectricVehicleOwner() {
+        ParkingSpot spot = spotOf(ParkingType.EV);
+        User nonEvOwner = new User(1L, "requester", false, true);
+        Reservation request = reservationOn(spot, BASE, BASE.plusHours(2), nonEvOwner);
+
+        assertThatExceptionOfType(ReservationPolicyViolationException.class)
+                .isThrownBy(() -> policy().validate(request, List.of()));
     }
 }
